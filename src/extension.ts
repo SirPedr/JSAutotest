a629cbc8fe05f43b908a29990da395348bfcd42d
SirPedr <ppagbpedro@gmail.com>
2022-11-13 17:15:50 -0300

    feat(commands): adding 'run tests' command to context menu

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,11 @@
-
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand("autotest.run", () => {
+    console.log("Opa");
+  });
 
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
