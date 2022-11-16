74e92728b1cf301460bf7f2740bdc69bc67a5767
SirPedr <ppagbpedro@gmail.com>
2022-11-15 21:07:28 -0300

    feat: running test script for selected file

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,15 @@
 import * as vscode from "vscode";
 
 export const activate = (context: vscode.ExtensionContext) => {
-  const disposable = vscode.commands.registerCommand("autotest.run", () => {
-    vscode.window.showInformationMessage("Acho que funciona");
-  });
+  const disposable = vscode.commands.registerCommand(
+    "autotest.run",
+    (uri: vscode.Uri) => {
+      const filePath = uri.path;
+      const terminal = vscode.window.createTerminal("Autotest");
+
+      terminal.sendText(`yarn test ${filePath}`);
+    }
+  );
 
   context.subscriptions.push(disposable);
 };
