4c05c2f9339140dc11c73e31f9d9028d9fe850e4
SirPedr <ppagbpedro@gmail.com>
2022-11-19 10:54:51 -0300

    feat(autotest.run): searching for test files on workspace when user runs tests from the implementation file

--- src/commands/run/index.ts
+++ src/commands/run/index.ts
@@ -1,12 +1,39 @@
+import path from "path";
+import minimatch from "minimatch";
 import * as vscode from "vscode";
 import { findTerminalWithName } from "../../lib/terminal/findTerminalWithName";
 import { createTestRunnerCommand } from "./util/createTestRunnerCommand";
 
 const commandName = "autotest.run";
+
 const DEFAULT_PACKAGE_MANAGER = "npm";
 const DEFAULT_SCRIPT_NAME = "test";
 
-const handler = (uri: vscode.Uri) => {
+const handler = async (uri: vscode.Uri) => {
+  const fileName = path.basename(uri.path);
+  const isTestFile = minimatch(fileName, "*.test.{js,jsx,ts,tsx}");
+
+  let testFilePath = uri.path;
+
+  if (!isTestFile) {
+    const [nameWithoutExtension] = fileName.split(".");
+    const [testFile] = await vscode.workspace.findFiles(
+      `**/${nameWithoutExtension}.test.{js,jsx,ts,tsx}`,
+      null,
+      1
+    );
+
+    if (!testFile) {
+      vscode.window.showErrorMessage(
+        `Couldn't find test file for ${fileName}.`
+      );
+
+      return;
+    }
+
+    testFilePath = testFile.path;
+  }
+
   const terminal = findTerminalWithName(vscode.window.terminals, "Autotest");
   const extensionConfig = vscode.workspace.getConfiguration("autotest");
 
@@ -14,7 +41,7 @@ const handler = (uri: vscode.Uri) => {
 
   terminal.sendText(
     createTestRunnerCommand({
-      filePath: uri.path,
+      filePath: testFilePath,
       packageManager: extensionConfig.get(
         "packageManager",
         DEFAULT_PACKAGE_MANAGER
